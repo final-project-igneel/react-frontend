f6fcba2e8d354429a0a059d02d393c99302e1b6e
aditilham <aditilham152@gmail.com>
2019-08-09 19:19:11 +0700

    improve my profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,17 +1,36 @@
 import React from "react";
 import NavBar from "./NavBar";
 import ProfilePhoto from "../images/Ellipse.png";
+import Swal from "sweetalert2";
 
 import axios from "axios";
+
+import { css } from "@emotion/core";
+import FadeLoader from "react-spinners/ScaleLoader";
+
+const override = css`
+  position: fixed;
+  top: 30vh;
+  left: 45vw;
+  border-color: red;
+`;
+
 
 export class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading:false,
       firstName: "",
       lastName: "",
       id: `${localStorage.getItem("user-id")}`
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: false
+    });
   }
 
   handleChange = event => {
@@ -30,6 +49,9 @@ export class UserProfile extends React.Component {
       .then(response => {
         console.log(response);
         if (response.status === 200) {
+          this.setState({
+            isLoading: true,
+          })
           window.localStorage.setItem(
             "user-firstName",
             JSON.stringify(response.data.editUser.firstName)
@@ -38,6 +60,14 @@ export class UserProfile extends React.Component {
             "user-lastName",
             JSON.stringify(response.data.editUser.lastName)
           );
+
+          Swal.fire({
+            title: "Success!",
+            text:
+              "Updated!",
+            type: "success",
+          });
+
           this.props.history.push("/Main");
         }
       })
@@ -46,116 +76,75 @@ export class UserProfile extends React.Component {
       });
   };
 
-  // render() {
-  //   return (
-  //     <div className="MyProfile">
-  //       <NavBar askButton="invisible" />
-  //       <div className="edit-content">
-  //       <div id="user-profile">
-  //       <img src={ProfilePhoto} id="profile-photo" alt="profile" />
-  //         <h2>
-  //           {localStorage.getItem("user-firstName").slice(1, -1)}{" "}
-  //           {localStorage.getItem("user-lastName").slice(1, -1)}
-  //         </h2>
-          
-  //         </div>
-          
-  //         <div id="edit-user-form">
-  //         <form onSubmit={this.handleSubmit}>
-  //           <div className="edit-user-form">
-  //             <h3>My Profile</h3>
-  //             <div className="inputbox-container">
-  //               <h5>First Name</h5>
-  //               <input
-  //                 className="inputbox"
-  //                 id="inputbox-userprofile"
-  //                 placeholder={localStorage
-  //                   .getItem("user-firstName")
-  //                   .slice(1, -1)}
-  //                 type="text"
-  //                 name="firstName"
-  //                 onChange={this.handleChange}
-  //               />
-  //             </div>
-  //             <div className="inputbox-container">
-  //               <h5>Last Name</h5>
-  //               <input
-  //                 className="inputbox"
-  //                 id="inputbox-userprofile"
-  //                 placeholder={localStorage
-  //                   .getItem("user-lastName")
-  //                   .slice(1, -1)}
-  //                 type="text"
-  //                 name="lastName"
-  //                 onChange={this.handleChange}
-  //               />
-  //               <button id="sign-up-button" type="submit">
-  //             Save!
-  //           </button>
-  //             </div>
-  //           </div>
-            
-  //         </form>
-  //         </div>
-  //       </div>
-        
-  //   </div>
-  //   );
-  // }
-
-
-render() {
-  return (
-    <div>
-      <NavBar askButton="invisible" />
-      <div>
-        <div id="profile-edit">
-        <img src={ProfilePhoto} id="profile-photo" alt="profile" />
-          <h2>
-            {localStorage.getItem("user-firstName").slice(1, -1)}{" "}
-            {localStorage.getItem("user-lastName").slice(1, -1)}
-          </h2>
+  render() {
+    if(this.state.isLoading){
+      return (
+        <div className="sweet-loading">
+          <FadeLoader
+            css={override}
+            sizeUnit={"px"}
+            height={150}
+            width={30}
+            color={"#BC4646"}
+            loading={this.state.loading}
+          />
         </div>
-        <div className='form-profile-edit'>
-        <form onSubmit={this.handleSubmit}>
-             <div className="edit-user-form">
-             <h3>Edit Profile Name</h3>
-             <div className="inputbox-editUser">
-                <h5>First Name</h5>
-                <input
-                 className="inputbox"
-                 id="inputbox-userprofile"
-                  placeholder={localStorage
-                    .getItem("user-firstName")
-                     .slice(1, -1)}
-                   type="text"
-                   name="firstName"
-                   onChange={this.handleChange}
-                 />
-               </div>
-               <div className="inputbox-editUser">
-                 <h5>Last Name</h5>
-                 <input
-                   className="inputbox"
-                   id="inputbox-userprofile"
-                   placeholder={localStorage
-                     .getItem("user-lastName")
-                     .slice(1, -1)}
-                   type="text"
-                   name="lastName"
-                   onChange={this.handleChange}
-                 />
-                 <button className="edit-button" type="submit">
-               Save!
-             </button>
-               </div>
-             </div>
-            
-           </form>
+      );
+    
+  } else {
+    return (
+      <div>
+        <NavBar askButton="invisible" />
+        <div>
+          <div id="profile-edit">
+            <img src={ProfilePhoto} id="profile-photo" alt="profile" />
+            <h2>
+              {localStorage.getItem("user-firstName").slice(1, -1)}{" "}
+              {localStorage.getItem("user-lastName").slice(1, -1)}
+            </h2>
+          </div>
+          <div className="form-profile-edit">
+            <form onSubmit={this.handleSubmit}>
+              <div className="edit-user-form">
+                <h3>Edit Profile Name</h3>
+                <div className="inputbox-editUser">
+                  <h5>First Name</h5>
+                  <input
+                    className="inputbox"
+                    id="inputbox-userprofile"
+                    placeholder={localStorage
+                      .getItem("user-firstName")
+                      .slice(1, -1)}
+                    type="text"
+                    name="firstName"
+                    onChange={this.handleChange}
+                    not
+                  />
+                </div>
+                <div className="inputbox-editUser">
+                  <h5>Last Name</h5>
+                  <input
+                    className="inputbox"
+                    id="inputbox-userprofile"
+                    placeholder={localStorage
+                      .getItem("user-lastName")
+                      .slice(1, -1)}
+                    type="text"
+                    name="lastName"
+                    onChange={this.handleChange}
+                  />
+                  <button className="edit-button" type="submit">
+                    Save!
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )}}
-
+    );
+  }
+}
+}
 
 export default UserProfile;
